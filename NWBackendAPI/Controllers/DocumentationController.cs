e0fa377151dfb27f2cf35edf704df3b87c90c02a
Valter Backström <dylandante@gmail.com>
2024-09-17 19:50:31 +0300

    keycode added to documentations

--- NWBackendAPI/Controllers/DocumentationController.cs
+++ NWBackendAPI/Controllers/DocumentationController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NWBackendAPI.Models;
 using System.Reflection.Metadata;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 namespace NWBackendAPI.Controllers
 {
@@ -36,6 +38,32 @@ namespace NWBackendAPI.Controllers
             catch (Exception ex)
             {
                 return BadRequest($"Error: {ex.Message}");
+            }
+        }
+
+        private readonly NorthwindOriginalContext _context = new();
+        private const string KeyCode = "123";
+
+        //public DocumentationController(NorthwindOriginalContext context)
+        //{
+        //    _context = context;
+        //}
+
+        [HttpGet("{keycode}")]
+        public ActionResult GetDocumentationByKeycode(string keycode)
+        {
+            if (keycode == KeyCode)
+            {
+                var documentation = _context.Documentations.ToList();
+                if (documentation.Any())
+                {
+                    return Ok(documentation);
+                }
+                return NotFound("Documentation is empty");
+            }
+            else
+            {
+                return NotFound($"Documentation missing at {DateTime.Now}");
             }
         }
 
